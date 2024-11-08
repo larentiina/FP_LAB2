48750f7e01154a41483dfd3f972b5714ab725094
Kuzenina Valeria <118663828+larentiina@users.noreply.github.com>
2024-11-08 14:49:42 +0300

    Update Tests.fs

--- LAB2.Tests/Tests.fs
+++ LAB2.Tests/Tests.fs
@@ -5,17 +5,16 @@ open Xunit
 open FsCheck
 open Dict
 
-// [<Fact>]
-// let ``Put Test`` () =
-//     let map = emptyMap
-//     let map1 = put 1 "value1" map
-//     let map2 = emptyMap |> put "second" "value1" 
+[<Fact>]
+let ``Put Test`` () =
+    let map = emptyMap
+    let map1 = put 1 "value1" map
+    let map2 = emptyMap |> put "second" "value1" 
    
 
-//     let result1 = get 1 map1
-//     let result2 = get "second" map2
-//     printfn "Checking if 'value1' exists in the bucket: %A" result1
-//     Assert.Equal(result1, result2)
+    let result1 = get 1 map1
+    let result2 = get "second" map2
+    Assert.Equal(result1, result2)
 
 [<Fact>]
 let ``Remove Test`` () =
@@ -24,40 +23,7 @@ let ``Remove Test`` () =
     let result = get 1 updatedMap
     Assert.Equal(result, None)
 
-// [<Fact>]
-// let ``Resize Test`` () =
 
-//     let map: HashMap<int, int> =
-//         [| [ { Key = 1; Value = 1 } ]
-//            [ { Key = 2; Value = 2 } ]
-//            [ { Key = 3; Value = 3 } ] |]
-
-//     let updatedMap = put  4 4 map
-
-//     let result = size updatedMap
-//     Assert.Equal(6, result)
-
-// [<Fact>]
-// let ``Merge Test`` () =
-//     let map1 = emptyMap |> put 1 "value1"
-//     let map2 = emptyMap |> put 1 "value2" |> put 2 "value2"
-//     let mergedMap = merge map1 map2
-//     let mergedBacket = mergedMap.[hash 1]
-
-//     let result1 =
-//         mergedBacket
-//         |> List.exists (fun entry -> entry.Value = "value1")
-
-//     let result2 =
-//         mergedBacket
-//         |> List.exists (fun entry -> entry.Value = "value2")
-
-//     printfn "Merged bucket contains the following entries:"
-
-//     mergedBacket
-//     |> List.iter (fun entry -> printfn "Key: %d, Value: %s" entry.Key entry.Value)
-
-//     Assert.Equal(true, result1 && result2)
 
 [<Fact>]
 let ``Filter Test`` () =
@@ -76,20 +42,16 @@ let ``Filter Test`` () =
 
 
 
-// [<Fact>]
-// let ``Map Function Test`` () =
-//     let map1 = emptyMap |> put 1 "value1" |> put 2 "value2"
+[<Fact>]
+let ``Map Function Test`` () =
+    let map1 = emptyMap |> put 1 "value1" |> put 2 "value2"
 
-//     let mapper value = value + "_mapped"
-//     let mappedMap = map mapper map1
+    let mapper value = value + "_mapped"
+    let mappedMap = map mapper map1
 
-//     let value1 = mappedMap.[hash 1]
-//     let value2 = mappedMap.[hash 2]
+    let expectedMap: HashMap<int, string> = emptyMap |> put 1 "value1_mapped" |> put 2 "value2_mapped"
 
-//     printfn "Mapped values: Key 1 = %s, Key 2 = %s" (List.head value1).Value (List.head value2).Value
-
-//     Assert.Equal("value1_mapped", (List.head value1).Value)
-//     Assert.Equal("value2_mapped", (List.head value2).Value)
+    Assert.Equal(true, equal mappedMap expectedMap)
 
 [<Fact>]
 let ``Fold Test <int,int>`` () =
