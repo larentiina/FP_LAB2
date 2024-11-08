a314420c9e395e4fdbf4e7f31b7646522accfe19
Kuzenina Valeria <118663828+larentiina@users.noreply.github.com>
2024-11-08 14:49:25 +0300

    Update PropertyTests.fs

--- LAB2.Tests/PropertyTests.fs
+++ LAB2.Tests/PropertyTests.fs
@@ -12,21 +12,15 @@ let genEntry (genKey: Gen<'Key>) (genValue: Gen<'Value>) : Gen<Entry<'Key, 'Valu
         return { Key = key; Value = value }
     }
 
-let hash (key: 'Key) (size: int) : int =
-    match box key with
-    | null -> 0
-    | _ -> abs (key.GetHashCode() % size)
-
-
-let genHashMap (genKey: Gen<'Key>) (genValue: Gen<'Value>) (size: int) : Gen<HashMap<'Key, 'Value>> =
+let genHashMap (genKey: Gen<'Key>) (genValue: Gen<'Value>) : Gen<HashMap<'Key, 'Value>> =
     gen {
         let hashMap = emptyMap
-        let! entries = Gen.listOf (genEntry genKey genValue)
+        let! entries = Gen.listOf(genEntry genKey genValue)
         return entries |> List.fold (fun acc entry -> put entry.Key entry.Value acc) hashMap
     }
 
 let arbHashMap<'Key, 'Value when 'Key : comparison> () : Arbitrary<HashMap<'Key, 'Value>> =
-    Arb.fromGen (genHashMap Arb.generate Arb.generate 5)
+    Arb.fromGen (genHashMap Arb.generate Arb.generate) 
 
 type HashMapGenerators =
     static member HashMapArbitrary<'Key, 'Value when 'Key : comparison>() : Arbitrary<HashMap<'Key, 'Value>> =
@@ -34,9 +28,30 @@ type HashMapGenerators =
 
 Arb.register<HashMapGenerators>() |> ignore
 
-[<Property(Arbitrary = [| typeof<HashMapGenerators> |])>]
-let prop_emptyMerge (map: HashMap<int, string>) =
-    let emptyMap = emptyMap
+
+let reinsertAll (hashMap: HashMap<'Key, 'Value>) : HashMap<'Key, 'Value> =
+    let empty = emptyMap
+    Array.fold (fun acc chain ->
+        List.fold (fun acc' entry ->
+            put entry.Key entry.Value acc') acc chain) empty hashMap
+
+[<Property(Verbose = true, Arbitrary = [| typeof<HashMapGenerators> |])>]
+let prop_emptyMerge (map: HashMap<int, int>) =
+    let map = reinsertAll map
     let mergedMap = merge map emptyMap
     equal map mergedMap 
 
+
+[<Property(Arbitrary = [| typeof<HashMapGenerators> |])>]
+let prop_association (map1: HashMap<int, int>) (map2:HashMap<int, int> ) (map3:HashMap<int, int> ) =
+    let mergedMap1 = merge(merge map1 map2) map3
+    let mergedMap2 = merge (merge map1 map3) map2
+    equal mergedMap1 mergedMap2 
+
+[<Property>]
+let prop_equalMaps (key1: int) (value1: int) (key2: int) (value2: int) =
+    key1 <> key2 ==> lazy (
+        let map1 = emptyMap |> put key1 value1 |> put key2 value2
+        let map2 = emptyMap |> put key2 value2 |> put key1 value1
+        equal map1 map2
+    )
